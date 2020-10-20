f464377de67c650fb22417c7c2972309b6ee6ad4
parallacks <epic.lancealot@gmail.com>
2020-10-19 19:27:35 -0600

    Added login and register modals

--- src/components/global/GamblerNavbar.vue
+++ src/components/global/GamblerNavbar.vue
@@ -1,56 +1,83 @@
 <template>
-  <b-navbar class="is-black">
-    <template slot="brand">
-      <b-navbar-item>
-        <!-- padding is all -->
-      </b-navbar-item>
-    </template>
-    <template slot="start">
-      <b-navbar-item>
-        <router-link to="/">
-          Home
-        </router-link>
-      </b-navbar-item>
-      <b-navbar-item>
-        <router-link to="Services">
-          Services
-        </router-link>
-      </b-navbar-item>
-      <b-navbar-item>
-        <router-link to="Upcoming">
-          Upcoming Trips
-        </router-link>
-      </b-navbar-item>
-      <b-navbar-item>
-        <router-link to="Contact">
-          Contact Us
-        </router-link>
-      </b-navbar-item>
-      <b-navbar-item>
-        <router-link to="Blog">
-          Blog
-        </router-link>
-      </b-navbar-item>
-    </template>
+  <div>
+    <b-navbar class="is-black">
+      <template slot="brand">
+        <b-navbar-item>
+          <!-- padding is all -->
+        </b-navbar-item>
+      </template>
+      <template slot="start">
+        <b-navbar-item>
+          <router-link to="/">
+            Home
+          </router-link>
+        </b-navbar-item>
+        <b-navbar-item>
+          <router-link to="Services">
+            Services
+          </router-link>
+        </b-navbar-item>
+        <b-navbar-item>
+          <router-link to="Upcoming">
+            Upcoming Trips
+          </router-link>
+        </b-navbar-item>
+        <b-navbar-item>
+          <router-link to="Contact">
+            Contact Us
+          </router-link>
+        </b-navbar-item>
+        <b-navbar-item>
+          <router-link to="Blog">
+            Blog
+          </router-link>
+        </b-navbar-item>
+      </template>
 
-    <template slot="end">
-      <b-navbar-item tag="div">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">
-            Log in
-          </a>
-        </div>
-      </b-navbar-item>
-    </template>
-  </b-navbar>
+      <template slot="end">
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <a class="button is-primary"
+               @click="registerModal()">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light"
+               @click="loginModal()">
+              Log in
+            </a>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
+import LoginModal from '@/components/global/Login'
+import RegisterModal from '@/components/global/Register'
 export default {
+  data() {
+    return {
+      isLoginActive: false
+    }
+  },
   methods: {
+    loginModal() {
+      this.$buefy.modal.open({
+        parent: this,
+        component: LoginModal,
+        hasModalCard: true,
+        customClass: 'custom-class custom-class-2',
+        trapFocus: true
+      })},
+    registerModal() {
+      this.$buefy.modal.open({
+        parent: this,
+        component: RegisterModal,
+        hasModalCard: true,
+        customClass: 'custom-class custom-class-2',
+        trapFocus: true
+      })}
   }
 }
 </script>
---
+++ src/components/global/Register.vue
@@ -0,0 +1,85 @@
+<template>
+  <form action="">
+    <div class="modal-card" style="width: auto">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Register</p>
+        <button
+          type="button"
+          class="delete"
+          @click="$emit('close')" />
+      </header>
+      <section class="modal-card-body">
+        <b-field v-if="isCompany" label="Company Name">
+          <b-input
+            :value="companyName"
+            placeholder="Company Name"
+            required />
+        </b-field>
+
+        <b-field label="First Name">
+          <b-input
+            :value="firstName"
+            placeholder="First Name"
+            required />
+        </b-field>
+
+        <b-field label="Last Name">
+          <b-input
+            type=""
+            :value="lastName"
+            placeholder="Last Name"
+            required />
+        </b-field>
+
+        <b-field label="Email">
+          <b-input
+            type="email"
+            :value="email"
+            placeholder="Your email"
+            required />
+        </b-field>
+
+        <b-field label="Password">
+          <b-input
+            type="password"
+            :value="password"
+            password-reveal
+            placeholder="Your password"
+            required />
+        </b-field>
+        <div class="block">
+          <b-radio v-model="isCompany"
+                   name="type"
+                   native-value="false">
+            Customer
+          </b-radio>
+          <b-radio v-model="isCompany"
+                   name="type"
+                   native-value="true">
+            Company
+          </b-radio>
+        </div>
+        <b-checkbox>Remember me</b-checkbox>
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button" type="button" @click="$emit('close')">Close</button>
+        <button class="button is-primary">Login</button>
+      </footer>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isCompany: false,
+      firstName: null,
+      lastName: null,
+      email: null,
+      password: null,
+      companyName: null
+    }
+  }
+}
+</script>
